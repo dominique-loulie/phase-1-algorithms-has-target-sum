43cd82074114ec48056e652de063ac92dfa9f6dd
Dominique Loulie <dom.loulie@gmail.com>
2023-05-27 13:26:00 +1000

    this is my first

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+// Runtime: O(n)
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+
+  return false;
+}
+
+// O(n) runtime
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
+
+// O(1) runtime
+function findSock(object) {
+  if (object.sock) return "sock";
 }
 
 /* 
   Write the Big O time complexity of your function here
+  => Runtime: O(n^2)
+  => Space: O(n)
 */
 
 /* 
@@ -12,6 +36,10 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  =>This object can be used to keep track of numbers we've already seen when iterating through an array. We start by looping through each number
+   in the array and then identify a complement that adds to the target (comp = target - num). Then, we check if any key on our object is equal to 
+   the complement. If so, it returns true; otherwise, it adds that number into the object. Finally, if no matches are found after reaching end of 
+   array iteration then false is returned.
 */
 
 // You can run `node index.js` to view these console logs
@@ -25,10 +53,22 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
+  //
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  //
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 7, 4], 6));
 }
 
 module.exports = hasTargetSum;
